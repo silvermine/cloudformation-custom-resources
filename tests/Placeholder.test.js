e350ae54f54d1acaf568ca0b99b485d3caad7ec1
Jeremy Thomerson <jeremy@thomersonfamily.com>
2018-04-07 11:51:09 -0500

    Initial commit - project stub

---
+++ tests/Placeholder.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Everything', function() {
+
+   it('needs to be tested', function() {
+      expect(true).to.strictlyEqual(true);
+   });
+
+});
