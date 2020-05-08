66cbf40e309d81b518f69cd332b6f7e411d653a5
Ethan Smith <ethan@onebytegone.com>
2020-05-08 15:19:07 -0400

    fix: only set BillingMode when it differs between src and dest
    Updating a GSI on a global table using provisioned capacity was failing with `One or more
    parameter values were invalid: ProvisionedThroughput must be specified when BillingMode is
    PROVISIONED`. This was due to BillingMode getting set on the table, but not
    ProvisionedThroughput. This commit fixes this as it makes the custom resource only set
    BillingMode when it actually has changed.

--- src/DynamoDBGlobalTable.js
+++ src/DynamoDBGlobalTable.js
@@ -353,7 +353,7 @@ module.exports = BaseResource.extend({
       // Similarly, we do not update the stream status because it should never change
       // after the initial creation since global tables require a specific type of stream.
 
-      if (srcBillingMode) {
+      if (srcBillingMode && srcBillingMode !== destBillingMode) {
          params.BillingMode = srcBillingMode;
       }
 
